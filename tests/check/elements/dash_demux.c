2f701781fd347ef570e13095a984153292670bdc
Florin Apostol <florin.apostol@oregan.net>
2016-01-11 19:03:59 +0000

    adaptivedemux: tests: test getting clock compensation using HTTP HEAD

--- tests/check/elements/dash_demux.c
+++ tests/check/elements/dash_demux.c
@@ -2869,6 +2869,135 @@ GST_START_TEST (testClockCompensationHttpXSdate)
 
 GST_END_TEST;
 
+static gboolean
+testClockCompensationHttpHead_http_src_start (GstTestHTTPSrc * src,
+    const gchar * uri, GstTestHTTPSrcInput * input_data, gpointer user_data)
+{
+  gboolean ret;
+
+  ret = gst_dashdemux_http_src_start (src, uri, input_data, user_data);
+  if (ret && g_strcmp0 (uri, "http://mocktime/http-head") == 0) {
+    GDateTime *now;
+    GDateTime *new_time;
+    gchar *date_str;
+    const GstDashDemuxTestInputData *test_input_data =
+        (const GstDashDemuxTestInputData *) input_data->context;
+
+    now = g_date_time_new_now_utc ();
+    fail_unless (now != NULL);
+    new_time =
+        g_date_time_add_seconds (now,
+        atoi ((const char *) test_input_data->payload));
+    fail_unless (new_time != NULL);
+    g_date_time_unref (now);
+    date_str = g_date_time_format (new_time, "%a, %e %b %Y %T %Z");
+    fail_unless (date_str != NULL);
+    g_date_time_unref (new_time);
+    input_data->response_headers =
+        gst_structure_new (TEST_HTTP_SRC_RESPONSE_HEADERS_NAME, "Date",
+        G_TYPE_STRING, date_str, NULL);
+    fail_unless (input_data->response_headers != NULL);
+    g_free (date_str);
+  }
+  return ret;
+}
+
+/*
+ * Test clock compensation during a live stream.
+ *
+ * There are 4 segments, 3s each.
+ * We set the mpd availability 6s before now.
+ * The server is 3-4s ahead of the client (HTTP HEAD does not retrieve
+ * milliseconds, so its precision is 1s), which means it is currently generating
+ * segment 4.
+ * We expect the client to download segment 4 and to wait for it to
+ * be available, so the test should take a little over 3s.
+ *
+ * Do not reduce the segment duration! When running under valgrind, the test
+ * will execute slower and dash demux might seek to a different segment
+ * when the manifest is received and the wallclock time is read.
+ */
+GST_START_TEST (testClockCompensationHttpHead)
+{
+  gchar *mpd;
+  const gchar *mpd_1 =
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+      "<MPD xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+      "     xmlns=\"urn:mpeg:DASH:schema:MPD:2011\""
+      "     xsi:schemaLocation=\"urn:mpeg:DASH:schema:MPD:2011 DASH-MPD.xsd\""
+      "     profiles=\"urn:mpeg:dash:profile:isoff-live:2011\""
+      "     type=\"dynamic\" availabilityStartTime=\"";
+  GDateTime *availabilityStartTime;
+  gchar *availabilityStartTimeString;
+  const gchar *mpd_2 = "\""
+      "     minBufferTime=\"PT1.500S\""
+      "     minimumUpdatePeriod=\"PT500S\">"
+      "  <UTCTiming schemeIdUri=\"urn:mpeg:dash:utc:http-head:2014\" value=\"http://mocktime/http-head\"/>"
+      "  <Period>"
+      "    <AdaptationSet mimeType=\"audio/webm\""
+      "                   subsegmentAlignment=\"true\">"
+      "      <Representation id=\"171\""
+      "                      codecs=\"vorbis\""
+      "                      audioSamplingRate=\"44100\""
+      "                      startWithSAP=\"1\""
+      "                      bandwidth=\"129553\">"
+      "        <AudioChannelConfiguration"
+      "           schemeIdUri=\"urn:mpeg:dash:23003:3:audio_channel_configuration:2011\""
+      "           value=\"2\" />"
+      "        <SegmentTemplate duration=\"3\""
+      "                         media=\"audio$Number$.webm\""
+      "                         >"
+      "        </SegmentTemplate>"
+      "      </Representation></AdaptationSet></Period></MPD>";
+
+  GstDashDemuxTestInputData inputTestData[] = {
+    {"http://unit.test/test.mpd", NULL, 0},
+    {"http://mocktime/http-head", (guint8 *) "4", 0},   /* server is 3-4s ahead */
+    {"http://unit.test/audio1.webm", NULL, 1000},
+    {"http://unit.test/audio2.webm", NULL, 2000},
+    {"http://unit.test/audio3.webm", NULL, 3000},
+    {"http://unit.test/audio4.webm", NULL, 4000},
+    {NULL, NULL, 0},
+  };
+  GstAdaptiveDemuxTestExpectedOutput outputTestData[] = {
+    {"audio_00", 4000, NULL},
+  };
+  GstTestHTTPSrcCallbacks http_src_callbacks = { 0 };
+  GstTestHTTPSrcTestData http_src_test_data = { 0 };
+  GstAdaptiveDemuxTestCallbacks test_callbacks = { 0 };
+  GstAdaptiveDemuxTestCase *testData;
+
+  availabilityStartTime = timeFromNow (-6);
+  availabilityStartTimeString = toXSDateTime (availabilityStartTime);
+  mpd = g_strdup_printf ("%s%s%s", mpd_1, availabilityStartTimeString, mpd_2);
+  g_free (availabilityStartTimeString);
+  inputTestData[0].payload = (guint8 *) mpd;
+
+  http_src_callbacks.src_start = testClockCompensationHttpHead_http_src_start;
+  http_src_callbacks.src_create = gst_dashdemux_http_src_create;
+  http_src_test_data.input = inputTestData;
+  gst_test_http_src_install_callbacks (&http_src_callbacks,
+      &http_src_test_data);
+
+  test_callbacks.appsink_received_data = testClockCompensationCheckDataReceived;
+  test_callbacks.appsink_eos = gst_adaptive_demux_test_unexpected_eos;
+
+  testData = gst_adaptive_demux_test_case_new ();
+  COPY_OUTPUT_TEST_DATA (outputTestData, testData);
+  testData->availabilityStartTime = availabilityStartTime;
+  testData->clockCompensation = 4;      /* server is 3-4s ahead */
+
+  gst_adaptive_demux_test_run (DEMUX_ELEMENT_NAME, "http://unit.test/test.mpd",
+      &test_callbacks, testData);
+
+  gst_object_unref (testData);
+  if (http_src_test_data.data)
+    gst_structure_free (http_src_test_data.data);
+  g_free (mpd);
+}
+
+GST_END_TEST;
+
 /*
  * Test clock compensation during a live stream.
  *
@@ -2999,6 +3128,7 @@ dash_demux_suite (void)
   tcase_add_test (tc_liveTests, testQueryLiveStream);
   tcase_add_test (tc_liveTests, testSeekLiveStream);
   tcase_add_test (tc_liveTests, testClockCompensationHttpXSdate);
+  tcase_add_test (tc_liveTests, testClockCompensationHttpHead);
   tcase_add_test (tc_liveTests, testClockCompensationHttpNtp);
 
   tcase_add_unchecked_fixture (tc_basicTests, gst_adaptive_demux_test_setup,
