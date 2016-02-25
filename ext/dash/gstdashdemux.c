asrashley/gst-plugins-bad

commit 3c2d839cb9fa1bc1a86922707558174281125700

adaptivedemux: use GstSystemClock to all real-time calculations
A realtime clock is used in many places, such as deciding which
fragment to select at start up and deciding how long to sleep
before a fragment becomes available. For example dashdemux needs
sample the client's estimate of UTC when selecting where to start
in a live DASH stream.

The problem with dashdemux calculating the client's idea of UTC is
that it makes it difficult to create unit tests, because the passage
of time is a factor in the test.

This commit changes dashdemux and adaptivedemux to use the
GstSystemClock, so that a unit test can replace the system clock when
it needs to be able to control the clock.

This commit makes no change to the behaviour under normal usage, as
GstSystemClock is based upon the system time.

ext/dash/gstdashdemux.c
@@ -266,7 +266,8 @@ static GstCaps *gst_dash_demux_get_input_caps (GstDashDemux * demux,
     GstActiveStream * stream);
 static GstPad *gst_dash_demux_create_pad (GstDashDemux * demux,
     GstActiveStream * stream);
-static GstDashDemuxClockDrift *gst_dash_demux_clock_drift_new (void);
+static GstDashDemuxClockDrift *gst_dash_demux_clock_drift_new (GstDashDemux *
+    demux);
 static void gst_dash_demux_clock_drift_free (GstDashDemuxClockDrift *);
 static gboolean gst_dash_demux_poll_clock_drift (GstDashDemux * demux);
 static GTimeSpan gst_dash_demux_get_clock_compensation (GstDashDemux * demux);
@@ -714,7 +715,7 @@ gst_dash_demux_setup_streams (GstAdaptiveDemux * demux)
           SUPPORTED_CLOCK_FORMATS, NULL);
       if (urls) {
         GST_DEBUG_OBJECT (dashdemux, "Found a supported UTCTiming element");
-        dashdemux->clock_drift = gst_dash_demux_clock_drift_new ();
+        dashdemux->clock_drift = gst_dash_demux_clock_drift_new (dashdemux);
         gst_dash_demux_poll_clock_drift (dashdemux);
       }
     }
@@ -1543,8 +1544,12 @@ gst_dash_demux_stream_get_fragment_waiting_time (GstAdaptiveDemuxStream *
     gint64 diff;
     GstDateTime *cur_time;
 
-    cur_time = gst_date_time_new_now_utc ();
-    diff = gst_mpd_client_calculate_time_difference (cur_time,
+    cur_time =
+        gst_date_time_new_from_g_date_time
+        (gst_adaptive_demux_get_client_now_utc (GST_ADAPTIVE_DEMUX_CAST
+            (dashdemux)));
+    diff =
+        gst_mpd_client_calculate_time_difference (cur_time,
         segmentAvailability);
     gst_date_time_unref (segmentAvailability);
     gst_date_time_unref (cur_time);
@@ -1728,13 +1733,15 @@ gst_dash_demux_stream_free (GstAdaptiveDemuxStream * stream)
 }
 
 static GstDashDemuxClockDrift *
-gst_dash_demux_clock_drift_new (void)
+gst_dash_demux_clock_drift_new (GstDashDemux * demux)
 {
   GstDashDemuxClockDrift *clock_drift;
 
   clock_drift = g_slice_new0 (GstDashDemuxClockDrift);
   g_mutex_init (&clock_drift->clock_lock);
-  clock_drift->next_update = g_get_monotonic_time ();
+  clock_drift->next_update =
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time
+      (GST_ADAPTIVE_DEMUX_CAST (demux)));
   return clock_drift;
 }
 
@@ -2023,7 +2030,9 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
   g_return_val_if_fail (demux != NULL, FALSE);
   g_return_val_if_fail (demux->clock_drift != NULL, FALSE);
   clock_drift = demux->clock_drift;
-  now = g_get_monotonic_time ();
+  now =
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time
+      (GST_ADAPTIVE_DEMUX_CAST (demux)));
   if (now < clock_drift->next_update) {
     /*TODO: If a fragment fails to download in adaptivedemux, it waits
        for a manifest reload before another attempt to fetch a fragment.
@@ -2053,7 +2062,8 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
       goto quit;
     }
   }
-  start = g_date_time_new_now_utc ();
+  start =
+      gst_adaptive_demux_get_client_now_utc (GST_ADAPTIVE_DEMUX_CAST (demux));
   if (!value) {
     GstFragment *download;
     gint64 range_start = 0, range_end = -1;
@@ -2081,7 +2091,7 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
         urls[clock_drift->selected_url]);
     goto quit;
   }
-  end = g_date_time_new_now_utc ();
+  end = gst_adaptive_demux_get_client_now_utc (GST_ADAPTIVE_DEMUX_CAST (demux));
   if (!value && method == GST_MPD_UTCTIMING_TYPE_HTTP_NTP) {
     value = gst_dash_demux_parse_http_ntp (clock_drift, buffer);
   } else if (!value) {
@@ -2158,8 +2168,13 @@ gst_dash_demux_get_clock_compensation (GstDashDemux * demux)
 static GDateTime *
 gst_dash_demux_get_server_now_utc (GstDashDemux * demux)
 {
-  GDateTime *client_now = g_date_time_new_now_utc ();
-  GDateTime *server_now = g_date_time_add (client_now,
+  GDateTime *client_now;
+  GDateTime *server_now;
+
+  client_now =
+      gst_adaptive_demux_get_client_now_utc (GST_ADAPTIVE_DEMUX_CAST (demux));
+  server_now =
+      g_date_time_add (client_now,
       gst_dash_demux_get_clock_compensation (demux));
   g_date_time_unref (client_now);
   return server_now;

gst-libs/gst/adaptivedemux/gstadaptivedemux.c
@@ -400,6 +400,8 @@ gst_adaptive_demux_init (GstAdaptiveDemux * demux,
     GstAdaptiveDemuxClass * klass)
 {
   GstPadTemplate *pad_template;
+  GstClockType clock_type = GST_CLOCK_TYPE_REALTIME;
+  GObjectClass *gobject_class;
 
   GST_DEBUG_OBJECT (demux, "gst_adaptive_demux_init");
 
@@ -413,6 +415,27 @@ gst_adaptive_demux_init (GstAdaptiveDemux * demux,
 
   gst_segment_init (&demux->segment, GST_FORMAT_TIME);
 
+  demux->realtime_clock = gst_system_clock_obtain ();
+  g_assert (demux->realtime_clock != NULL);
+  gobject_class = G_OBJECT_GET_CLASS (demux->realtime_clock);
+  if (g_object_class_find_property (gobject_class, "clock-type")) {
+    g_object_get (demux->realtime_clock, "clock-type", &clock_type, NULL);
+  }
+  if (clock_type == GST_CLOCK_TYPE_REALTIME) {
+    demux->clock_offset = 0;
+  } else {
+    GDateTime *utc_now;
+    GstClockTime rtc_now;
+    GTimeVal gtv;
+
+    utc_now = g_date_time_new_now_utc ();
+    rtc_now = gst_clock_get_time (demux->realtime_clock);
+    g_date_time_to_timeval (utc_now, &gtv);
+    demux->clock_offset =
+        gtv.tv_sec * G_TIME_SPAN_SECOND + gtv.tv_usec -
+        GST_TIME_AS_USECONDS (rtc_now);
+    g_date_time_unref (utc_now);
+  }
   g_rec_mutex_init (&demux->priv->updates_lock);
   demux->priv->updates_task =
       gst_task_new ((GstTaskFunction) gst_adaptive_demux_updates_loop,
@@ -466,6 +489,10 @@ gst_adaptive_demux_finalize (GObject * object)
   g_rec_mutex_clear (&demux->priv->manifest_lock);
   g_mutex_clear (&demux->priv->api_lock);
   g_mutex_clear (&demux->priv->segment_lock);
+  if (demux->realtime_clock) {
+    gst_object_unref (demux->realtime_clock);
+    demux->realtime_clock = NULL;
+  }
 
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
@@ -2060,7 +2087,8 @@ _src_chain (GstPad * pad, GstObject * parent, GstBuffer * buffer)
   }
 
   stream->download_total_time +=
-      g_get_monotonic_time () - stream->download_chunk_start_time;
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux)) -
+      stream->download_chunk_start_time;
   stream->download_total_bytes += gst_buffer_get_size (buffer);
 
   gst_adapter_push (stream->adapter, buffer);
@@ -2081,7 +2109,8 @@ _src_chain (GstPad * pad, GstObject * parent, GstBuffer * buffer)
     g_mutex_unlock (&stream->fragment_download_lock);
   }
 
-  stream->download_chunk_start_time = g_get_monotonic_time ();
+  stream->download_chunk_start_time =
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux));
 
   if (ret != GST_FLOW_OK) {
     if (ret < GST_FLOW_EOS) {
@@ -2453,8 +2482,9 @@ gst_adaptive_demux_stream_download_uri (GstAdaptiveDemux * demux,
     }
 
     if (G_LIKELY (stream->last_ret == GST_FLOW_OK)) {
-      stream->download_start_time = g_get_monotonic_time ();
-      stream->download_chunk_start_time = g_get_monotonic_time ();
+      stream->download_start_time =
+          GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux));
+      stream->download_chunk_start_time = stream->download_start_time;
 
       /* src element is in state READY. Before we start it, we reset
        * download_finished
@@ -2658,7 +2688,8 @@ static void
 gst_adaptive_demux_stream_download_loop (GstAdaptiveDemuxStream * stream)
 {
   GstAdaptiveDemux *demux = stream->demux;
-  guint64 next_download = g_get_monotonic_time ();
+  guint64 next_download =
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux));
   GstFlowReturn ret;
   gboolean live;
 
@@ -2800,7 +2831,9 @@ gst_adaptive_demux_stream_download_loop (GstAdaptiveDemuxStream * stream)
       gint64 wait_time =
           gst_adaptive_demux_stream_get_fragment_waiting_time (demux, stream);
       if (wait_time > 0) {
-        gint64 end_time = g_get_monotonic_time () + wait_time / GST_USECOND;
+        gint64 end_time =
+            GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux))
+            + wait_time / GST_USECOND;
 
         GST_DEBUG_OBJECT (stream->pad, "Download waiting for %" GST_TIME_FORMAT,
             GST_TIME_ARGS (wait_time));
@@ -2834,7 +2867,8 @@ gst_adaptive_demux_stream_download_loop (GstAdaptiveDemuxStream * stream)
 
     stream->last_ret = GST_FLOW_OK;
 
-    next_download = g_get_monotonic_time ();
+    next_download =
+        GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux));
     ret = gst_adaptive_demux_stream_download_fragment (stream);
 
     if (ret == GST_FLOW_FLUSHING) {
@@ -3027,7 +3061,8 @@ gst_adaptive_demux_updates_loop (GstAdaptiveDemux * demux)
 
   GST_MANIFEST_LOCK (demux);
 
-  next_update = g_get_monotonic_time () +
+  next_update =
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux)) +
       klass->get_manifest_update_interval (demux);
 
   /* Updating playlist only needed for live playlists */
@@ -3068,8 +3103,9 @@ gst_adaptive_demux_updates_loop (GstAdaptiveDemux * demux)
       demux->priv->update_failed_count++;
       if (demux->priv->update_failed_count <= DEFAULT_FAILED_COUNT) {
         GST_WARNING_OBJECT (demux, "Could not update the playlist");
-        next_update = g_get_monotonic_time () +
-            klass->get_manifest_update_interval (demux);
+        next_update =
+            GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux))
+            + klass->get_manifest_update_interval (demux);
       } else {
         GST_ELEMENT_ERROR (demux, STREAM, FAILED,
             (_("Internal data stream error.")), ("Could not update playlist"));
@@ -3080,7 +3116,8 @@ gst_adaptive_demux_updates_loop (GstAdaptiveDemux * demux)
       }
     } else {
       GST_DEBUG_OBJECT (demux, "Updated playlist successfully");
-      next_update = g_get_monotonic_time () +
+      next_update =
+          GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux)) +
           klass->get_manifest_update_interval (demux);
 
       /* Wake up download tasks */
@@ -3228,7 +3265,7 @@ gst_adaptive_demux_stream_advance_fragment_unlocked (GstAdaptiveDemux * demux,
   }
 
   stream->download_start_time = stream->download_chunk_start_time =
-      g_get_monotonic_time ();
+      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time (demux));
 
   if (ret == GST_FLOW_OK) {
     if (gst_adaptive_demux_stream_select_bitrate (demux, stream,
@@ -3407,4 +3444,36 @@ gst_adaptive_demux_advance_period (GstAdaptiveDemux * demux)
   klass->advance_period (demux);
   gst_adaptive_demux_expose_streams (demux, FALSE);
   gst_adaptive_demux_start_tasks (demux);
+}
+
+/**
+ * gst_adaptive_demux_get_monotonic_time:
+ * Returns: a monotonically increasing time, using the system realtime clock
+ */
+GstClockTime
+gst_adaptive_demux_get_monotonic_time (GstAdaptiveDemux * demux)
+{
+  g_return_val_if_fail (demux != NULL, GST_CLOCK_TIME_NONE);
+  return gst_clock_get_time (demux->realtime_clock);
+}
+
+/**
+ * gst_adaptive_demux_get_client_now_utc:
+ * @demux: #GstAdaptiveDemux
+ * Returns: the client's estimate of UTC
+ *
+ * Used to find the client's estimate of UTC, using the system realtime clock.
+ */
+GDateTime *
+gst_adaptive_demux_get_client_now_utc (GstAdaptiveDemux * demux)
+{
+  GstClockTime rtc_now;
+  gint64 utc_now;
+  GTimeVal gtv;
+
+  rtc_now = gst_clock_get_time (demux->realtime_clock);
+  utc_now = demux->clock_offset + GST_TIME_AS_USECONDS (rtc_now);
+  gtv.tv_sec = utc_now / G_TIME_SPAN_SECOND;
+  gtv.tv_usec = utc_now % G_TIME_SPAN_SECOND;
+  return g_date_time_new_from_timeval_utc (&gtv);
 }

gst-libs/gst/adaptivedemux/gstadaptivedemux.h
@@ -205,6 +205,10 @@ struct _GstAdaptiveDemux
   gboolean have_group_id;
   guint group_id;
 
+  /* Realtime clock */
+  GstClock *realtime_clock;
+  gint64 clock_offset; /* offset between realtime_clock and UTC (in usec) */
+
   /* < private > */
   GstAdaptiveDemuxPrivate *priv;
 };
@@ -449,6 +453,9 @@ gst_adaptive_demux_stream_advance_fragment (GstAdaptiveDemux * demux,
     GstAdaptiveDemuxStream * stream, GstClockTime duration);
 void gst_adaptive_demux_stream_queue_event (GstAdaptiveDemuxStream * stream,
     GstEvent * event);
+
+GstClockTime gst_adaptive_demux_get_monotonic_time (GstAdaptiveDemux * demux);
+GDateTime *gst_adaptive_demux_get_client_now_utc (GstAdaptiveDemux * demux);
 
 G_END_DECLS
 

commit 7f656937b12f0648af3fde2e0e0b8bdac100607f

dashdemux: fixup of polling UTC timing
The commit 3c2d839cb9fa1bc1a86922707558174281125700
(adaptivedemux: use GstSystemClock to all real-time calculations)
introduced a regression in the clock drift compensation, that causes
the timing source to be polled every time the manifest is refreshed.
If the manifest has a very small minimumUpdatePeriod, this can cause
significant numbers of requests to the clock server.

The commit fixes this issue, and makes the code slightly easier to
follow by using GstClockTime for all of its calculations, rather than
using microseconds for the next_update.

ext/dash/gstdashdemux.c
@@ -200,8 +200,8 @@ enum
 #define DEFAULT_PRESENTATION_DELAY     NULL     /* zero */
 
 /* Clock drift compensation for live streams */
-#define SLOW_CLOCK_UPDATE_INTERVAL  (1000000 * 30 * 60) /* 30 minutes */
-#define FAST_CLOCK_UPDATE_INTERVAL  (1000000 * 30)      /* 30 seconds */
+#define SLOW_CLOCK_UPDATE_INTERVAL  (GST_SECOND * 30 * 60)      /* 30 minutes */
+#define FAST_CLOCK_UPDATE_INTERVAL  (GST_SECOND * 30)   /* 30 seconds */
 #define SUPPORTED_CLOCK_FORMATS (GST_MPD_UTCTIMING_TYPE_NTP | GST_MPD_UTCTIMING_TYPE_HTTP_HEAD | GST_MPD_UTCTIMING_TYPE_HTTP_XSDATE | GST_MPD_UTCTIMING_TYPE_HTTP_ISO | GST_MPD_UTCTIMING_TYPE_HTTP_NTP)
 #define NTP_TO_UNIX_EPOCH G_GUINT64_CONSTANT(2208988800)        /* difference (in seconds) between NTP epoch and Unix epoch */
 
@@ -210,7 +210,7 @@ struct _GstDashDemuxClockDrift
   GMutex clock_lock;            /* used to protect access to struct */
   gchar *selected_url;
   GstMPDUTCTimingType selected_method;
-  gint64 next_update;
+  GstClockTime next_update;
   /* @clock_compensation: amount (in usecs) to add to client's idea of
      now to map it to the server's idea of now */
   GTimeSpan clock_compensation;
@@ -1772,8 +1772,7 @@ gst_dash_demux_clock_drift_new (GstDashDemux * demux)
   clock_drift = g_slice_new0 (GstDashDemuxClockDrift);
   g_mutex_init (&clock_drift->clock_lock);
   clock_drift->next_update =
-      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time
-      (GST_ADAPTIVE_DEMUX_CAST (demux)));
+      gst_adaptive_demux_get_monotonic_time (GST_ADAPTIVE_DEMUX_CAST (demux));
   clock_drift->selected_method = SUPPORTED_CLOCK_FORMATS;
   return clock_drift;
 }
@@ -2046,7 +2045,7 @@ static gboolean
 gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
 {
   GstDashDemuxClockDrift *clock_drift;
-  gint64 now;
+  GstClockTime now;
   GstMPDUTCTimingType method;
   gchar **urls;
   guint current_index, i, leng;
@@ -2055,19 +2054,22 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
   g_return_val_if_fail (demux != NULL, FALSE);
   g_return_val_if_fail (demux->clock_drift != NULL, FALSE);
   clock_drift = demux->clock_drift;
-  now =
-      GST_TIME_AS_USECONDS (gst_adaptive_demux_get_monotonic_time
-      (GST_ADAPTIVE_DEMUX_CAST (demux)));
+  now = gst_adaptive_demux_get_monotonic_time (GST_ADAPTIVE_DEMUX_CAST (demux));
   g_mutex_lock (&clock_drift->clock_lock);
   urls = gst_mpd_client_get_utc_timing_sources (demux->client,
       clock_drift->selected_method, &method);
   if (!urls && clock_drift->selected_method != SUPPORTED_CLOCK_FORMATS) {
     /* If the currently selected method was removed during a manifest
        update, try switching to another method */
-    urls = gst_mpd_client_get_utc_timing_sources (demux->client,
+    GST_INFO_OBJECT (demux,
+        "Currently selected UTC timing method 0x%x is no longer listed in the manifest, searching for a new method",
+        clock_drift->selected_method);
+    urls =
+        gst_mpd_client_get_utc_timing_sources (demux->client,
         SUPPORTED_CLOCK_FORMATS & ~clock_drift->selected_method, &method);
   }
   if (!urls) {
+    GST_WARNING_OBJECT (demux, "Unable to find a supported UTC timing source");
     goto fail;
   }
 
@@ -2081,6 +2083,13 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
   }
   if (clock_drift->selected_url == NULL || !urls[current_index]
       || method != clock_drift->selected_method) {
+#ifndef GST_DISABLE_GST_DEBUG
+    if (clock_drift->selected_url) {
+      GST_INFO_OBJECT (demux,
+          "Previous UTC timing source \"%s\" no longer listed in manifest, selecting new source",
+          clock_drift->selected_url);
+    }
+#endif
     current_index = g_random_int_range (0, leng);
     reset = TRUE;
   }
@@ -2112,9 +2121,9 @@ gst_dash_demux_poll_clock_drift (GstDashDemux * demux)
     }
     if (gst_dash_demux_poll_clock_drift_server (demux)) {
       clock_drift->next_update =
-          now + (method ==
-          GST_MPD_UTCTIMING_TYPE_NTP) ? FAST_CLOCK_UPDATE_INTERVAL :
-          SLOW_CLOCK_UPDATE_INTERVAL;
+          now + ((method ==
+              GST_MPD_UTCTIMING_TYPE_NTP) ? (FAST_CLOCK_UPDATE_INTERVAL) :
+          (SLOW_CLOCK_UPDATE_INTERVAL));
       g_mutex_unlock (&clock_drift->clock_lock);
       return TRUE;
     }